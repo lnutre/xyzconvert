89672ed905f4b2a557571236eb9d590b1ae629bf
yansh <1963964882@qq.com>
2024-05-05 12:59:23 +0800

    完善

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-from csv import writer, reader
-from os import path, replace
-from re import split
-from sys import argv
-
+import argparse
+from os import path
+import numpy as np
 from chardet.universaldetector import UniversalDetector
+
 
 def Get_encoding(filepath):
     detector = UniversalDetector()
     for line in open(filepath, 'rb'):
         detector.feed(line)
-        if detector.done: break
+        if detector.done:
+            break
     detector.close()
     return detector.result['encoding']
 
-def savepath(openpath: str):
+
+def savefile(data, openpath: str, mode):
     file_dir, filename = path.split(openpath)
     filename, file_ext = path.splitext(filename)
-    return str(path.join(file_dir, filename))
+    file_ext = '.' + mode
+    np.savetxt(str(path.join(path.join(file_dir, filename), file_ext)), data, delimiter=',', fmt="%.2f")
+    return str(path.join(path.join(file_dir, filename), file_ext))
 
+
+def openfile(openpath: str):
+    data = np.loadtxt(openpath, delimiter=" ,\t", encoding=Get_encoding(openpath))
+    data = data[:, -3:-1]
+    return data
 
 
 if __name__ == "__main__":
+    parse = argparse.ArgumentParser()
+    parse.add_argument("filepath", help="文件路径")
+    parse.add_argument("-mode", help="转换类型", choices=['xyz', 'dat'])
+    parse.add_argument("--c", help="调整数值")
+    args = parse.parse_args()
+    if args.filepath:
+        if args.c:
+            print(0)
+        else:
+            savefile(openfile(args.filepath), args.filepath, args.mode)
+    else:
+        print("请输入文件路径")
