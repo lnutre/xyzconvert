f2b58e3ba027ac500b498ba0a0746dff908b3490
yansh <1963964882@qq.com>
2025-07-10 22:01:03 +0800

    修改win11为二级快捷方式

--- menu.py
+++ menu.py
@@ -3,60 +3,69 @@ import winreg as reg
 from os import path
 import platform
 
-def add_context_menu(menu_name, command, reg_root_key_path, reg_key_path, shortcut_key):
+def add_context_menu_parent(parent_name, reg_root_key_path, reg_key_path, shortcut_key):
     """
-    封装的添加一个右键菜单的方法
-    :param menu_name: 显示的菜单名称
-    :param command: 菜单执行的命令
+    添加一级右键菜单
+    :param parent_name: 一级菜单名称
     :param reg_root_key_path: 注册表根键路径
-    :param reg_key_path: 要添加到的注册表父键的路径（相对路径）
-    :param shortcut_key: 菜单快捷键，如：'S'
-    :return:
+    :param reg_key_path: 要添加到的注册表父键的路径
+    :param icon_path: 菜单图标路径
+    :param shortcut_key: 菜单快捷键
     """
-    # 打开名称父键
-    key = reg.OpenKey(reg_root_key_path, reg_key_path)
-    # 为key创建一个名称为menu_name的sub_key，并设置sub_key的值为menu_name加上快捷键，数据类型为REG_SZ字符串类型
-    reg.SetValue(key, menu_name, reg.REG_SZ, menu_name + '(&{0})'.format(shortcut_key))
-
-    # 打开刚刚创建的名为menu_name的sub_key
-    sub_key = reg.OpenKey(key, menu_name)
-    # 为sub_key添加名为'command'的子键，并设置其值为command + ' "%v"'，数据类型为REG_SZ字符串类型
-    reg.SetValue(sub_key, 'command', reg.REG_SZ, command + ' "%v"')
-
-    # 关闭sub_key和key
-    reg.CloseKey(sub_key)
+    try:
+        key = reg.OpenKey(reg_root_key_path, reg_key_path, 0, reg.KEY_ALL_ACCESS)
+    except Exception as msg:
+        key = reg.CreateKey(reg_root_key_path, reg_key_path)
+    reg.SetValueEx(key, 'MUIVerb', 0, reg.REG_SZ, parent_name + '(&{0})'.format(shortcut_key))
+    reg.SetValueEx(key, 'Default', 0, reg.REG_SZ, '')
+    reg.SetValueEx(key, 'SubCommands', 0, reg.REG_SZ, '')
     reg.CloseKey(key)
 
+def add_context_menu(menu_name, command, reg_root_key_path, reg_key_path):
+    """
+    添加二级右键菜单项
+    :param menu_name: 二级菜单名称
+    :param command: 菜单执行的命令
+    :param reg_root_key_path: 注册表根键路径
+    :param reg_key_path: 要添加到的注册表父键的路径
+    :param icon_path: 菜单图标路径
+    """
+    try:
+        key = reg.OpenKey(reg_root_key_path, reg_key_path,0, reg.KEY_ALL_ACCESS)
+    except Exception as msg:
+        key = reg.CreateKey(reg_root_key_path, reg_key_path)
+    reg.SetValueEx(key, 'MUIVerb', 0, reg.REG_SZ, menu_name)
+    reg.SetValue(key, 'command', reg.REG_SZ, command+ ' "%v"')
+    reg.CloseKey(key)
 
-def delete_reg_key(root_key, key, menu_name):
+def delete_reg_key(root_key, sub_key):
     """
     删除一个右键菜单注册表子键
     :param root_key:根键
-    :param key: 父键
-    :param menu_name: 菜单子键名称
+    :param sub_key: 父键
     :return: None
     """
     try:
-        parent_key = reg.OpenKey(root_key, key)
-    except Exception as msg:
-        print(msg)
-        return
-    if parent_key:
-        try:
-            menu_key = reg.OpenKey(parent_key, menu_name)
-        except Exception as msg:
-            print(msg)
-            return
-        if menu_key:
-            try:
-                # 必须先删除子键的子键，才能删除子键本身
-                reg.DeleteKey(menu_key, 'command')
-            except Exception as msg:
-                print(msg)
-                return
-            else:
-                reg.DeleteKey(parent_key, menu_name)
+        # 打开指定的注册表键
+        with reg.OpenKey(root_key, sub_key, 0, reg.KEY_ALL_ACCESS) as key:
+            # 获取子键数量
+            info = reg.QueryInfoKey(key)
+            num_sub_keys = info[0]
 
+            # 递归删除所有子键
+            for i in range(num_sub_keys - 1, -1, -1):
+                sub_key_name = reg.EnumKey(key, i)
+                delete_reg_key(root_key, f"{sub_key}\\{sub_key_name}")
+
+        # 删除当前键
+        reg.DeleteKey(root_key, sub_key)
+        print(f"删除: {sub_key}")
+    except FileNotFoundError:
+        print(f"该y {sub_key} 不存在.")
+    except PermissionError:
+        print(f"你没有权限删除 {sub_key}.请以管理员身份运行")
+    except Exception as e:
+        print(f"删除时发生错误 {sub_key}: {e}")
 
 def add_menu(z: float):
     """
@@ -65,21 +74,26 @@ def add_menu(z: float):
     """
     if platform.system() == 'Windows':
         if int(platform.version().split('.')[2]) >= 22000:
-            return
-        else:
-            # 添加文件右键菜单
-            add_context_menu('转换为XYZ', path.dirname(__file__) + '\main.exe -mode xyz', reg.HKEY_CLASSES_ROOT, r'*\\shell',
-                             'S')
-            add_context_menu('转换为DAT', path.dirname(__file__) + '\main.exe -mode dat', reg.HKEY_CLASSES_ROOT, r'*\\shell',
-                             'S')
+            add_context_menu_parent('xyz转换工具', reg.HKEY_CLASSES_ROOT, r'*\\shell\\xyzcon', 'C')
+            add_context_menu('转换为XYZ', path.dirname(__file__) + '\main.exe -mode xyz', reg.HKEY_CLASSES_ROOT, r'*\\shell\\xyzcon\\shell\\2xyz')
+            add_context_menu('转换为DAT', path.dirname(__file__) + '\main.exe -mode dat', reg.HKEY_CLASSES_ROOT, r'*\\shell\\xyzcon\\shell\\2dat')
             add_context_menu('转换为XYZ并转换高程', path.dirname(__file__) + '\main.exe -mode xyz --c {}'.format(z),
                              reg.HKEY_CLASSES_ROOT,
-                             r'*\\shell',
-                             'S')
+                             r'*\\shell\\xyzcon\\shell\\2h')
             add_context_menu('交换XY', path.dirname(__file__) + '\main.exe -mode xyz --swap 1',
                              reg.HKEY_CLASSES_ROOT,
-                             r'*\\shell',
-                             'S')
+                             r'*\\shell\\xyzcon\\shell\\swapxy')
+
+        else:
+            # 添加文件右键菜单
+            add_context_menu('转换为XYZ', path.dirname(__file__) + '\main.exe -mode xyz', reg.HKEY_CLASSES_ROOT, r'*\\shell\\转换为XYZ')
+            add_context_menu('转换为DAT', path.dirname(__file__) + '\main.exe -mode dat', reg.HKEY_CLASSES_ROOT, r'*\\shell\\转换为DAT')
+            add_context_menu('转换为XYZ并转换高程', path.dirname(__file__) + '\main.exe -mode xyz --c {}'.format(z),
+                             reg.HKEY_CLASSES_ROOT,
+                             r'*\\shell\\转换为XYZ并转换高程')
+            add_context_menu('交换XY', path.dirname(__file__) + '\main.exe -mode xyz --swap 1',
+                             reg.HKEY_CLASSES_ROOT,
+                             r'*\\shell\\交换XY')
     # 添加文件夹右键菜单
     # add_context_menu(menu_name, py_command, reg.HKEY_CLASSES_ROOT, r'Directory\\shell', 'S')
     # 添加文件夹空白处右键菜单
@@ -89,9 +103,13 @@ def add_menu(z: float):
 
 
 def delete_all_menu():
-    menu_name = ['转换为DAT', '转换为XYZ', '转换为XYZ并转换高程', '交换XY']
-    for i in menu_name:
-        delete_reg_key(reg.HKEY_CLASSES_ROOT, r'*\\shell', i)
-        # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Directory\\shell', menu_name)
-        # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Directory\\Background\\shell', menu_name)
-        # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Drive\\shell', menu_name)
+    if platform.system() == 'Windows':
+        if int(platform.version().split('.')[2]) >= 22000:
+            delete_reg_key(reg.HKEY_CLASSES_ROOT, r'*\\shell\\xyzcon')
+        else:
+            menu_name = ['转换为DAT', '转换为XYZ', '转换为XYZ并转换高程', '交换XY']
+            for i in menu_name:
+                delete_reg_key(reg.HKEY_CLASSES_ROOT, r'*\\shell'+i)
+                # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Directory\\shell', menu_name)
+                # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Directory\\Background\\shell', menu_name)
+                # delete_reg_key(reg.HKEY_CLASSES_ROOT, r'Drive\\shell', menu_name)
